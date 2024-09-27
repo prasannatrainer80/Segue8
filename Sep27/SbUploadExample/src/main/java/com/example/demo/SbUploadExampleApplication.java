bf7c44c23bd8cc07b21c917230d61af067243286
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2024-09-27 16:16:03 +0530

    upload

---
+++ Sep27/SbUploadExample/src/main/java/com/example/demo/SbUploadExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbUploadExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbUploadExampleApplication.class, args);
+	}
+
+}
